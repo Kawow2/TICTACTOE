2878c76ce6a6b6f14eca00aa636d54fe3bc9f5f3
AnToine BIGEARD <antoine.bigeard.tpe@gmail.com>
2024-07-15 23:26:05 +0200

    feat: Avancement vi toire et fin partie

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-let counter = 0;
+let counter;
 
-Array.from(document.getElementsByClassName("box")).forEach(function (item) {
-  item.addEventListener("click", function functionToRemove() {
-    addImage(item, counter % 2 == 0 ? "circle" : "cross");
-    this.removeEventListener("click", functionToRemove);
-    checkWinner();
+startGame();
+
+//RESET
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener("click", function (item) {
+  Array.from(document.getElementsByClassName("box")).forEach(function (item) {
+    if (item.childNodes[0] != null)
+      {
+        item.removeChild(item.childNodes[0])
+      }
   });
+  startGame();
 });
 
+function startGame()
+{
+  Array.from(document.getElementsByClassName("box")).forEach(function (item) {
+    item.classList.remove("cross");
+    item.classList.remove("circle");
+    item.addEventListener("click", function functionToRemove() {
+      let type = counter % 2 == 0 ? "circle" : "cross"
+      addImage(item, type);
+      this.removeEventListener("click", functionToRemove);
+      winner = checkWinner(type);
+    });
+  });
+  counter = 0;
+}
+  
 function addImage(box, type) {
   let img = document.createElement("img");
   img.src = "images/" + type + ".png";
@@ -18,14 +39,74 @@ function addImage(box, type) {
   counter++;
 }
 
-function checkWinner() {
-  let boxes = Array.from(document.getElementsByClassName("box"));
-  let winner = false;
-  console.log(boxes);
+/*CLEAR BOARD */
+
+/*CHECK WINNERS*/
+function checkWinner(type) {
+  let boxes = document.getElementsByClassName("box");
+  let winnerRows = checkWinnerRows(boxes,type);
+  let winnerColumns = checkWinnerColums(boxes,type);
+  let winnerDiagonals = checkWinnerDiagonals(boxes,type)
+  if (winnerRows || winnerColumns || winnerDiagonals)
+    DeclareWinner(type); 
+  let gameEnded = checkGameEnded(boxes);
+  if (gameEnded)
+    DeclareWinner(type);
 }
 
-// function checkWinnerRows()
-// [
-//   let boxes = Array.from(document.getElementsByClassName("box"));
-//   if (boxes.)
-// ]
+function checkGameEnded(boxes)
+{
+  let i =0;
+  while (i<9)
+    {
+      
+    }
+}
+
+///
+function checkWinnerRows(boxes,type)
+{
+  let winner = false;
+  let count = 0;
+  while (winner == false && count < 9)
+  {
+    console.log(CheckTypeOnClassList(type,boxes[count].classList))
+    winner = CheckTypeOnClassList(type,boxes[count].classList) && CheckTypeOnClassList(type,boxes[count+1].classList) && CheckTypeOnClassList(type,boxes[count+2].classList); 
+    count+=3;
+  }
+  return winner;
+}
+
+function checkWinnerColums(boxes,type)
+{
+  let winner = false;
+  let count = 0;
+  while (winner == false && count < 3)
+  {
+    winner = CheckTypeOnClassList(type,boxes[count].classList) && CheckTypeOnClassList(type,boxes[count+3].classList) && CheckTypeOnClassList(type,boxes[count+6].classList); 
+    count+=3;
+  }
+  return winner;
+}
+
+function checkWinnerDiagonals(boxes,type)
+{
+  let firstDiagonal = CheckTypeOnClassList(type,boxes[0].classList) && CheckTypeOnClassList(type,boxes[4].classList) && CheckTypeOnClassList(type,boxes[8].classList);
+  let secondDiagonal = CheckTypeOnClassList(type,boxes[2].classList) && CheckTypeOnClassList(type,boxes[4].classList) && CheckTypeOnClassList(type,boxes[6].classList);
+  return firstDiagonal || secondDiagonal;
+  
+}
+
+function CheckTypeOnClassList(type, classList)
+{
+  return classList.contains(type);
+}
+
+function DeclareWinner(type)
+{
+  alert(type + "winner !!!")
+}
+function DeclareGameEnded()
+{
+  alert("Fin de partie...")
+}
